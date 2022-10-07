54203783a1beeff89e3c8116e0c980602c3c73e1
Waqas <waqasyaqoob8752@gmail.com>
2022-10-07 18:45:29 +0500

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+
 const forecast = (latitude,longitude, callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=ee1a13c8dddefc117e4f51e73a431b69&query=' + longitude + ',' + latitude  + '&units=f'
     request({ url, json:true }, (error, {body})=>{
@@ -8,7 +9,7 @@ const forecast = (latitude,longitude, callback)=>{
             callback('Unable to find location', undefined)
         }else{
             callback(undefined,
-            body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degree out.')
+            body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degree out. The humidity is ' + body.current.humidity + '%')
 
         }
     })
