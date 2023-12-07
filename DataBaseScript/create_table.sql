e9e3dd3d19322e3948bfb91e6e3662f79ff01799
waqasyaqoob87 <92632689+waqasyaqoob87@users.noreply.github.com>
2023-12-07 13:23:26 +0500

    Add files via upload

---
+++ DataBaseScript/create_table.sql
@@ -0,0 +1,6 @@
+-- create_table.sql
+
+CREATE TABLE example_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
---
+++ DataBaseScript/insert_data.sql
@@ -0,0 +1,7 @@
+-- insert_data.sql
+
+INSERT INTO example_table (id, name)
+VALUES
+    (1, 'John Doe'),
+    (2, 'Jane Smith'),
+    (3, 'Bob Johnson');
